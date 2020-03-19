e8aeaca3bb2f8720152ab40cb7c84f9c9a97a042
chaosannals <chaosannals@outlook.com>
2020-03-19 16:19:21 +0800

    diff

--- main/array-v-set.js
+++ main/array-v-set.js
@@ -5,10 +5,18 @@ let sb = randomSet(100000, 1, 10000000);
 let aa = Array.from(sa);
 let ab = Array.from(sb);
 
-timing('array', () => {
+timing('array intersect', () => {
     return aa.filter(i => ab.indexOf(i) >= 0);
 });
 
-timing('set', () => {
+timing('set intersect', () => {
     return Array.from(sa).filter(i => sb.has(i));
+});
+
+timing('array diff', () => {
+    return aa.filter(i => ab.indexOf(i) < 0);
+});
+
+timing('set diff', () => {
+    return Array.from(sa).filter(i => !sb.has(i));
 });
