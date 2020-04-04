7613d8cd5b6ae93f4353a78c47bed1a48c021f1f
chaosannals <chaosannals@outlook.com>
2020-04-04 10:33:25 +0800

    提取出。

--- main/array-v-set.js
+++ main/array-v-set.js
@@ -1,53 +1,30 @@
-import { timing, randomSet } from './common.js';
+import {
+    timing,
+    randomSet,
+    arrayIntersect,
+    arrayDiff,
+    arrayMerge
+} from './common.js';
 
-let sa = randomSet(1000000, 1, 10000000);
-let sb = randomSet(1000000, 1, 10000000);
-let aa = Array.from(sa).sort();
-let ab = Array.from(sb).sort();
+const sa = randomSet(1000000, 1, 10000000);
+const sb = randomSet(1000000, 1, 10000000);
+const aa = Array.from(sa).sort((a, b) => a - b);
+const ab = Array.from(sb).sort((a, b) => a - b);
 
 let ai = timing('array intersect', () => {
-    let result = [];
-    let i = 0;
-    let j = 0;
-    while (i < aa.length && j < ab.length) {
-        if (aa[i] < ab[j]) {
-            ++i;
-        } else if (aa[i] > ab[j]) {
-            ++j;
-        } else {
-            result.push(aa[i]);
-            ++i;
-            ++j;
-        }
-    }
-    return result;
+    return arrayIntersect(aa, ab);
 });
 console.log(`array size: ${ai.length}`);
 
 let si = timing('set intersect', () => {
-    let result = Array.from(sa).filter(i => sb.has(i));
-    return new Set(result);
+    let result = new Set();
+    sa.forEach(i => sb.has(i) && result.add(i));
+    return result;
 });
 console.log(`set size: ${si.size}`);
 
 let ad = timing('array diff', () => {
-    let result = [];
-    let i = 0;
-    let j = 0;
-    while (i < aa.length && j < ab.length) {
-        if (aa[i] < ab[j]) {
-            result.push(aa[i++]);
-        } else if(aa[i] > ab[j]) {
-            ++j;
-        } else {
-            ++i;
-            ++j;
-        }
-    }
-    while (i < aa.length) {
-        result.push(aa[i++]);
-    }
-    return result;
+    return arrayDiff(aa, ab);
 });
 console.log(`array size: ${ad.length}`);
 
@@ -58,31 +35,7 @@ let sd = timing('set diff', () => {
 console.log(`set size: ${sd.size}`);
 
 let am = timing('array merge', () => {
-    let result = [];
-    let i = 0;
-    let j = 0;
-    while (true) {
-        if (i >= aa.length) {
-            while (j < ab.length) {
-                result.push(ab[j++]);
-            }
-            return result;
-        }
-        if (j >= ab.length) {
-            while (i < aa.length) {
-                result.push(aa[i++]);
-            }
-            return result;
-        }
-        if (aa[i] < ab[j]) {
-            result.push(aa[i++]);
-        } else if (aa[i] > ab[j]) {
-            result.push(ab[j++]);
-        } else {
-            result.push(aa[i++]);
-            ++j;
-        }
-    }
+    return arrayMerge(aa, ab);
 });
 console.log(`array size: ${am.length}`);
 
--- main/common.js
+++ main/common.js
@@ -20,3 +20,70 @@ export function randomSet(length, min, max) {
     }
     return result;
 }
+
+export function arrayIntersect(a, b) {
+    let result = [];
+    let i = 0;
+    let j = 0;
+    while (i < a.length && j < b.length) {
+        if (a[i] < b[j]) {
+            ++i;
+        } else if (a[i] > b[j]) {
+            ++j;
+        } else {
+            result.push(a[i]);
+            ++i;
+            ++j;
+        }
+    }
+    return result;
+}
+
+export function arrayDiff(a, b) {
+    let result = [];
+    let i = 0;
+    let j = 0;
+    while (i < a.length && j < b.length) {
+        if (a[i] < b[j]) {
+            result.push(a[i++]);
+        } else if (a[i] > b[j]) {
+            ++j;
+        } else {
+            ++i;
+            ++j;
+        }
+    }
+    while (i < a.length) {
+        result.push(a[i++]);
+    }
+    return result;
+}
+
+export function arrayMerge(a, b) {
+    let result = [];
+    let i = 0;
+    let j = 0;
+    while (true) {
+        if (i >= a.length) {
+            while (j < b.length) {
+                result.push(b[j++]);
+            }
+            return result;
+        }
+        if (j >= b.length) {
+            while (i < a.length) {
+                result.push(a[i++]);
+            }
+            return result;
+        }
+        if (a[i] < b[j]) {
+            result.push(a[i++]);
+        } else if (a[i] > b[j]) {
+            result.push(b[j++]);
+        } else {
+            result.push(a[i]);
+            ++i;
+            ++j;
+        }
+    }
+}
