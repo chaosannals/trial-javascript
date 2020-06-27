4540e5cd39faa01baec23775a06f2ed78887cb41
chaosannals <chaosannals@outlook.com>
2020-06-27 14:29:55 +0800

    sql.js

--- main/dbsql.js
+++ main/dbsql.js
@@ -1,15 +1,18 @@
 import fs from 'fs';
 import initSqlJs from 'sql.js';
 
-initSqlJs().then(SQL => {
+function createDb(SQL) {
     let db = new SQL.Database();
     db.run("CREATE TABLE test(col1, col2);");
     let data = db.export();
     let buffer = Buffer.from(data);
     fs.writeFileSync("test.db", buffer);
-})
+}
 
 initSqlJs().then(SQL => {
+    if (!fs.existsSync("test.db")) {
+        createDb(SQL);
+    }
     let filebuffer = fs.readFileSync('test.db');
     let db = new SQL.Database(filebuffer);
 });
