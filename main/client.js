ab5580ec6bf0ef689a5f79bbae93ff67b9961ce8
chaosannals <chaosannals@outlook.com>
2020-07-10 11:37:21 +0800

    字节序

--- main/client.js
+++ main/client.js
@@ -1,7 +1,6 @@
-import {Socket} from 'net'
+import { Socket } from 'net'
 
 let client = new Socket();
-client.setEncoding('utf8');
 client.on('data', chunk => {
     console.log(chunk);
 })
@@ -10,5 +9,9 @@ client.on('error', e => {
 })
 client.connect(8000, 'localhost');
 setInterval(() => {
-    client.write(new Date().toString());
+    let buffer = Buffer.alloc(8);
+    buffer.writeUInt16BE(0xA00A, 0);
+    buffer.writeUInt32BE(1234, 2);
+    buffer.writeUInt16BE(0x0AA0, 6);
+    client.write(buffer);
 }, 1000);
