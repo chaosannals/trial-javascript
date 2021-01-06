a20bacda34353ee0550c43f42911edfac9e20ae0
chaosannals <chaosannals@outlook.com>
2021-01-06 16:11:32 +0800

    前端服务器微调。

--- view/app.js
+++ view/app.js
@@ -4,5 +4,6 @@ import serve from 'koa-static';
 
 const root = path.dirname(path.dirname(process.argv[1]));
 const app = new Koa();
+console.log(`http://localhost:3000`)
 app.use(serve(path.resolve(root, 'browser')));
 app.listen(3000);
