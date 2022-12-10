9747f4ddc0e3996a08b428918ddfe967242b5022
chaosannals <chaosannals@outlook.com>
2022-12-10 16:18:57 +0800

    save

---
+++ apov3demo/readme.md
@@ -0,0 +1,4 @@
+# Vue 3 + TypeScript + Vite + ApolloClient
+
+- VsCode 插件 [Apollo GraphQL](https://marketplace.visualstudio.com/items?itemName=apollographql.vscode-apollo) 
+
---
+++ apov3demo/src/main.ts
@@ -0,0 +1,19 @@
+import { createApp } from 'vue';
+import './style.css';
+import App from './App.vue';
+import { ApolloClient, InMemoryCache } from '@apollo/client/core';
+import { createApolloProvider } from '@vue/apollo-option';
+
+const cache = new InMemoryCache()
+const apolloClient = new ApolloClient({
+    cache,
+    uri: 'http://localhost:3000/graphql',
+});
+
+const apolloProvider = createApolloProvider({
+    defaultClient: apolloClient,
+});
+
+const app = createApp(App);
+app.use(apolloProvider);
+app.mount('#app');
